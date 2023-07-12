f9abf71fe8547e96411a49ed9a0c3d75f6628719
brevis-ng <brevisnguyen@gmail.com>
2023-07-12 23:55:02 +0800

    update order msg

--- resources/views/messages/order.blade.php
+++ resources/views/messages/order.blade.php
@@ -1,15 +1,15 @@
-<b>Thông tin đơn hàng</b><br>
-👤 Người nhận: <b>{{ $customer->name }}</b><br>
-📞 SĐT: <b>{{ $customer->phone }}</b><br>
-📦 Địa chỉ: <b>{{ $customer->address }}</b><br><br>
-🛒<b>Đơn hàng</b><br>
+<b>Thông tin đơn hàng {{ $order->id }}</b><br>
+👤 Người nhận: <code>{{ $customer->name }}</code><br>
+📞 SĐT: <code>{{ $customer->phone }}</code><br>
+📦 Địa chỉ: <code>{{ $customer->address }}</code><br><br>
+🛒<b>Sản phẩm</b><br>
 @forelse ($items as $item)
 <?php
     $name = $item->product['code'].'-'.$item->product['name'];
-    $max_length = 30;
+    $max_length = 20;
     $name = Illuminate\Support\Str::of($name)->padRight($max_length, '.')->limit($max_length);
 ?>
-<code>•{{ $name . '  x' . $item->quantity . '  ' . $item->amount . config('clover.currency') }}</code><br>
+<code>• {{ $name . '  x' . $item->quantity . '  ' . $item->amount . config('clover.currency') }}</code><br>
 @empty
 @endforelse
 🛵 Phí ship: {{ $order->shipping_amount . config('clover.currency') }}<br>
