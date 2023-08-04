663541fd59608a0b63b41853880635b3053e847e
brevis-ng <brevisnguyen@gmail.com>
2023-08-04 09:47:26 +0800

    Fix after create admin issue

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -6,6 +6,7 @@ use App\Enums\Roles;
 use App\Filament\Resources\UserResource;
 use App\Models\User;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Log;
 use Nutgram\Laravel\Facades\Telegram;
 use SergiX44\Nutgram\Telegram\Types\Command\BotCommand;
 use SergiX44\Nutgram\Telegram\Types\Command\BotCommandScopeChat;
@@ -16,19 +17,27 @@ class CreateUser extends CreateRecord
 
     protected function afterCreate(): void
     {
-        if ($this->data["role"] == Roles::ADMIN) {
-            Telegram::deleteMyCommands(
-                new BotCommandScopeChat($this->data["telegram_id"])
-            );
-            Telegram::setMyCommands(
-                [
-                    BotCommand::make("order", "Manage order /order orderNo"),
-                    BotCommand::make("cache", "Clear cache"),
-                    BotCommand::make("help", "Help message"),
-                    BotCommand::make("cancel", "Cancel"),
-                ],
-                new BotCommandScopeChat($this->data["telegram_id"])
-            );
+        try {
+            if ($this->data["role"] == Roles::ADMIN->value) {
+                Telegram::deleteMyCommands(
+                    new BotCommandScopeChat($this->data["telegram_id"])
+                );
+                Telegram::setMyCommands(
+                    [
+                        BotCommand::make("order", "Manage order /order orderNo"),
+                        BotCommand::make("cache", "Clear cache"),
+                        BotCommand::make("help", "Help message"),
+                        BotCommand::make("cancel", "Cancel"),
+                    ],
+                    new BotCommandScopeChat($this->data["telegram_id"])
+                );
+            }
+        } catch (\Throwable $e) {
+            Log::critical("{class} Error in line {line}: {message}", [
+                "class" => self::class,
+                "line" => $e->getLine(),
+                "message" => $e->getMessage(),
+            ]);
         }
     }
 }
