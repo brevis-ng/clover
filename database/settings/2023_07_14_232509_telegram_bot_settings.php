b80ab512ed9e1ab0584311958816759227610d2b
brevis-ng <brevisnguyen@gmail.com>
2023-08-21 11:38:16 +0800

    Fix security

--- database/settings/2023_07_14_232509_telegram_bot_settings.php
+++ database/settings/2023_07_14_232509_telegram_bot_settings.php
@@ -6,7 +6,7 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add("telegram.bot_token", "5931240678:AAGE40ErCdxa7xzKxUr5sYHVq0RLSH4nTu0");
+        $this->migrator->add("telegram.bot_token", "");
         $this->migrator->add("telegram.bot_username", "cloverbot");
         $this->migrator->add("telegram.webapp_inline_button", "Order Food");
         $this->migrator->add("telegram.webapp_menu_button", "Order Food");
